74435eba73d0fca48542e1e944c8baefcd47c582
Steven Fabricio Gazo Maliaño <steven.gazo@hotmail.com>
2022-06-15 20:36:28 -0600

    eliminar bordes y creación funcinoes js

--- js/index.js
+++ js/index.js
@@ -1,43 +1,66 @@
 
-
 // Arreglo con las rutas de los elementos
-let imagesPaths = ["./images/ant.jpg","./images/cat.jpg","./images/cow.jpg","./images/elephant.jpg"];
-
+let imagesPaths = ["./images/ant.jpg",
+                    "./images/ant.jpg",
+                    "./images/cat.jpg",
+                    "./images/cow.jpg",
+                    "./images/dog.jpg",
+                    "./images/elephant.jpg",
+                    "./images/fish.jpg",
+                    "./images/horse.jpg",
+                    "./images/lion.jpg",
+                    "./images/mouse.jpg",
+                    "./images/pig.jpg",
+                    "./images/rabbit.jpg",
+                    "./images/rooster.jpg",
+                    "./images/tiger.jpg",
+                    "./images/turtle.jpg",
+                    "./images/zebra.jpg",];
+                    
+// Contenedor donde es renderizada la imagen
 const imgContainer = document.getElementById("imgContainer");
 
-let counterImages = imagesPaths.length;
-let counter = 0;
 
-/*
-    la estructura (elemento)=>{} es una funcion anonima
-    es como hacer
-        function xxxxx(elemento){
+/* Retorna un numero aleatorio con base en alguna longitud dada del arreglo */
+function randomNumber(arrayLetgh){
+    const number = Math.floor(Math.random()* (arrayLetgh + 1));
+    console.log(`El numero aleatorio es ${number}`);
+    return number();
+}
 
-        }
-    sirve para declarar una función en sitio. El return es para cambiar el valor especifico que se esta procesando
-*/
+
+
 
 // la funcion .map recorre todos los elementos en un arreglo y los cambia conforme a la funcion
-var sample = imagesPaths.map(
+// CREA ARREGLO DE IMAGENES
+var arrayOfimages = imagesPaths.map(
     (ELEMENT)=>{
         var newImg = new Image(100,100);
         newImg.src= ELEMENT;
-        newImg.className="img-animal";
-        newImg.style.zIndex=5;
-        console.log(Element);
+        newImg.className="img-animal";                
         return newImg;
     }
 );
+console.log(arrayOfimages);
 
-function smpl(){
-    console.log("disparo");
-    while(counter<counterImages){
-        counter ++;
-        var tmpIMG = sample[counter];
-        tmpIMG.style.zIndex = counter+5;
-        imgContainer.appendChild(tmpIMG);
-    }
+
+
+
+
+/* Función para obteneer el nombre del animal */
+function getNameOfAnimal(animalImage){    
+    const src = animalImage.src;   
+    var name = src.substring(
+        src.lastIndexOf("/")+1,
+        src.lastIndexOf(".jpg")
+    );
+    return name;
 }
 
+
+for (let index = 0; index < arrayOfimages.length; index++) {
+    var name1 = getNameOfAnimal(arrayOfimages[index]);    
+    console.log(name1);
+}
 
 
