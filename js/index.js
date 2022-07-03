faa777e208224de0c7ea450d386f724d98bd65a9
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2022-07-02 21:47:05 -0600

    finalizacion de proyecto

--- js/index.js
+++ js/index.js
@@ -18,7 +18,6 @@ let imagesPaths = [
   "./images/zebra.jpg",
 ];
 
-
 let player = {
   name: "sample",
   points: 0,
@@ -69,8 +68,6 @@ function getNameOfAnimal(animalImage) {
   return name;
 }
 
-
-
 /*
   Recibe un objeto button y comprueba que el nombre sea igual
   al de la imagen seleccionada
@@ -78,12 +75,12 @@ function getNameOfAnimal(animalImage) {
 function validarNombre(boton) {
   const nombreAnimal = boton.innerText;
 
-  button1.style.pointerEvents= "none";
-  button2.style.pointerEvents= "none";
-  button3.style.pointerEvents= "none";
-  button4.style.pointerEvents= "none";
+  button1.style.pointerEvents = "none";
+  button2.style.pointerEvents = "none";
+  button3.style.pointerEvents = "none";
+  button4.style.pointerEvents = "none";
 
-  if (nombreAnimal === NombreImagenSeleccionada) {  
+  if (nombreAnimal === NombreImagenSeleccionada) {
     player.points++;
     // muestra el cuadro de exito
     divFelicitaciones.style.display = "flex";
@@ -91,56 +88,79 @@ function validarNombre(boton) {
     player.points--;
     /// muestra el cuadrito de error
     divError.style.display = "flex";
-    document.getElementById("error-title").innerText= `Error!!, the name is ${NombreImagenSeleccionada}`;
+    document.getElementById(
+      "error-title"
+    ).innerText = `Error!!, the name is ${NombreImagenSeleccionada}`;
   }
   playerPoints.value = player.points;
 
-  /// Mover a el boton continuar 
+  /// Mover a el boton continuar
   //RenderizarIMG();
-
 }
 
 /*
   Genera y asigna los nombres a los botones en pantalla
 */
-function GenerarNombresBotones(nombreCorrecto) {
-  var ArregloDeNombres = [];
-  ArregloDeNombres.push(nombreCorrecto);
-  for (let index = 0; index <= 2; index++) {
-    ArregloDeNombres.push(getNameOfAnimal(arrayOfimages[randomNumber(15)]));
-  }
+function GenerarNombresBotones(correctAnswer) {
+  var arrNamesOfAnimal = []; // valor temporal donde se  almacenaran los nombres
+  arrNamesOfAnimal.push(correctAnswer); // Mete la respuesta correcta
+  do {
+    // Selecciona un valor random  para los botones
+    var tmpValue = getNameOfAnimal(
+      arrayOfimages[randomNumber(arrayOfimages.length - 1)]
+    );
+    // Comprueba si el valor ya se encuentra en el arreglo
+    var flagFound = arrNamesOfAnimal.find((element) => element === tmpValue);
+    //Si no lo encuentra, lo agrega al arreglo
+    if (flagFound == undefined) {
+      arrNamesOfAnimal.push(tmpValue);
+    }
+  } while (arrNamesOfAnimal.length < 4); // Sigue iterando hasta que el arreglo tenga 4 elementos
 
-  for (let index = 0; index <= 3; index++) {
+  // Inserta 4 elementos
+  for (let i = 0; i <= 3; i++) {
+    var tmpRandom = Math.floor(Math.random() * (arrNamesOfAnimal.length - 1)); // Genera un dato aleatorio
+    var tmpValueToInser = arrNamesOfAnimal[tmpRandom]; // Setea un valor del arreglo como general
+    if (arrNamesOfAnimal.length === 1) {
+      tmpValueToInser = arrNamesOfAnimal[0];
+    }
     const random = randomNumber(4);
-
-    switch (index) {
+    debugger;
+    switch (i) {
       case 0:
-        button1.innerText = ArregloDeNombres[index];
+        button1.innerText = tmpValueToInser;
         break;
       case 1:
-        button2.innerText = ArregloDeNombres[index];
+        button2.innerText = tmpValueToInser;
         break;
       case 2:
-        button3.innerText = ArregloDeNombres[index];
+        button3.innerText = tmpValueToInser;
         break;
       case 3:
-        button4.innerText = ArregloDeNombres[index];
+        button4.innerText = tmpValueToInser;
         break;
       default:
         break;
     }
+    // estas lineas buscan en el arreglo el elemento y lo borra, reduciendo el arreglo
+    var index = arrNamesOfAnimal.indexOf(tmpValueToInser);
+    if (index > -10) {
+      arrNamesOfAnimal.splice(index, 1);
+    }
   }
 }
 
-const initPlay = ()=>{
-    document.getElementById("gameContainer").style.display="flex";
-    player.name = document.getElementById("playerName").value;
-    document.getElementById("titleUser").innerText = `Hello ${player.name} \n What is this?`;
-    RenderizarIMG();
+// Para el boton Play, este inicia el juego
+const initPlay = () => {
+  document.getElementById("gameContainer").style.display = "flex";
+  player.name = document.getElementById("playerName").value;
+  document.getElementById(
+    "titleUser"
+  ).innerText = `Hello ${player.name} \n What is this?`;
+  RenderizarIMG();
+};
 
-}
-
-
+/// Boton que sirve para cambiar y renderizar la imagen seleccionad
 function RenderizarIMG() {
   var img = arrayOfimages[counter];
   ImagenSeleccionada = img;
@@ -150,40 +170,30 @@ function RenderizarIMG() {
   img.style.zIndex = 4;
   img.className = "img-animal";
   imgContainer.appendChild(img);
-  counter++;  
-  
+  counter++;
 }
 
-function noneFunction (){
+function noneFunction() {}
 
-}
-
-
-function btnContinue(){
+/// funcion para el boton continuar
+function btnContinue() {
   cleanIMG();
   //cleanIMG();
   divFelicitaciones.style.display = "none";
   divError.style.display = "none";
 
-  button1.style.pointerEvents= "auto";
-  button2.style.pointerEvents= "auto";
-  button3.style.pointerEvents= "auto";
-  button4.style.pointerEvents= "auto";
-
-
+  button1.style.pointerEvents = "auto";
+  button2.style.pointerEvents = "auto";
+  button3.style.pointerEvents = "auto";
+  button4.style.pointerEvents = "auto";
   RenderizarIMG();
-
-
-
 }
 
 // Borra las imagenes del sitio
 function cleanIMG() {
   // Prueba remover elemeto
   const list = document.getElementsByTagName("img");
-  for (let index = 0; index < list.length; index++) {
-    imgContainer.removeChild(list[index]);
+  for (let ij = 0; ij < list.length; ij++) {
+    imgContainer.removeChild(list[ij]);
   }
 }
-
-initPlay();
